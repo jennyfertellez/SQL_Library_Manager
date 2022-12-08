b1ef9bdb515949999380c2d169a923c46fc7bdd5
jennyfertellez <jennyfertellez@gmail.com>
2022-12-08 01:22:49 -0800

    updated info

---
+++ models/book.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+//Book Model
+module.exports = ( sequelize ) => {
+  class Book extends Sequelize.Model {}
+  Book.init({
+    //Model attributes are defined here
+    id: {
+      type: Sequelize.Model,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false, //disallow null
+      validate: {
+        notEmpty: {
+          msg: 'Please provide a value for "Title"', //Ensures a TITLE is included
+        },
+      },
+    },
+    author: {
+      type: Sequelize.STRING,
+      allowNull: false, //disallow null
+      validate: {
+        notEmpty: {
+          msg: 'Please provide a value for "Author"', //Ensures an AUTHOR is included
+        },
+      },
+    },
+    genre: {
+      type: Sequelize.STRING
+    },
+    year: {
+      type: Sequelize.INTEGER
+  }, 
+}, {sequelize});
+
+  return Book;
+};
