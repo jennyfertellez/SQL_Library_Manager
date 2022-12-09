4b8de02916dbe99d45bd92033288e8826fbda528
jennyfertellez <jennyfertellez@gmail.com>
2022-12-08 18:13:19 -0800

    imported book model and added new book form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+//Import Book Model
+var { Book } = require('../models');
+var book = require('../models/book').default;
+
+//Handler function to wrap each route.
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error) {
+      next(error);
+    }
+  }
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+//Create a new book form 
+router.get('/books/new', (req, res) => {
+  res.render("books/new-book", { book: {}, title: "Add A New Book"});
+})
