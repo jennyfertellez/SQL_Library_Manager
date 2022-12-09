feda4bec60691ca5a8dc20d12d3ccc2194e96d05
jennyfertellez <jennyfertellez@gmail.com>
2022-12-08 19:17:39 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var { Book } = require('../models');
 var book = require('../models/book').default;
 
-//Handler function to wrap each route.
+//Handler function to wrap each route. (Middleware)
 function asyncHandler(cb) {
   return async(req, res, next) => {
     try {
@@ -15,14 +15,50 @@ function asyncHandler(cb) {
     }
   }
 }
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+//Home route redirecting to the /books rroute
+router.get('/', asyncHandler(async (req, res) => {
+  res.redirect('/books')
+}));
+
+//Shows the full list of books and stores them in a variable
+router.get('/books', asyncHandler(async (req, res) => {
+  const books = await Book.findAll({ order: [['createdAt', 'DESC']] });
+  res.render('index', { books, title: 'Library Books' });
+}));
 
 //Create a new book form 
 router.get('/books/new', (req, res) => {
   res.render("books/new-book", { book: {}, title: "Add A New Book"});
-})
+});
+
+//Posts a new book to the database
+router.post('/books/new', asyncHandler(async (req, res) => {
+  let book;
+  try {
+
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+
+    } else {
+      throw error;
+    }
+  }
+}));
+
+//Shows book detail form
+router.get('/books/:id', asyncHandler(async (req, res, next) => {
+  res.render("books/update-book", { book, title: book.title});
+}))
+
+//Updates book info in the databse
+router.post('/books/:id', asyncHandler(async (req, res) => {
+
+}))
+
+//Deletes a book
+router.post('/books/:id/delete', asyncHandler(async (req, res) => {
+
+}))
+
+module.exports = router;
